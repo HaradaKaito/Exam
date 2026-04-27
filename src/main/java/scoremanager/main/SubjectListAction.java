cb37bace493a4c47e0439a260768c30ac687f35f
harasan1016 <tks2573403@stu.o-hara.ac.jp>
2026-04-27 14:44:17 +0900

    プロジェクト新規作成

---
+++ src/main/java/scoremanager/main/SubjectListAction.java
@@ -0,0 +1,33 @@
+package scoremanager.main;
+
+import java.util.List;
+
+import bean.Subject;
+import bean.Teacher;
+import dao.SubjectDao;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import tool.Action;
+
+public class SubjectListAction extends Action {
+
+    @Override
+    public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+
+        HttpSession session = req.getSession();
+        Teacher teacher = (Teacher) session.getAttribute("user");
+
+        // DAO
+        SubjectDao subjectDao = new SubjectDao();
+
+        // 科目一覧を取得（学校コードで絞る）
+        List<Subject> subjects = subjectDao.filter(teacher.getSchool());
+
+        // JSP にセット
+        req.setAttribute("subjects", subjects);
+
+        // フォワード
+        req.getRequestDispatcher("subject_list.jsp").forward(req, res);
+    }
+}
