7c9d51361055b5fb2a9d38c84dd0e3dd1beb0251
h_kubota <tks2573512@stu.o-hara.ac.jp>
2026-05-08 11:23:02 +0900

    科目削除

---
+++ src/main/java/scoremanager/main/SubjectDeleteAction.java
@@ -0,0 +1,29 @@
+package scoremanager.main;
+
+import dao.SubjectDao;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import tool.Action;
+
+public class SubjectDeleteAction extends Action {
+
+    // @Override 外す
+    public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        // 科目コード取得
+        String cd = request.getParameter("cd");
+
+        // null対策
+        if (cd == null || cd.isEmpty()) {
+            response.sendRedirect("SubjectList.action");
+            return;
+        }
+
+        // 削除実行
+        SubjectDao dao = new SubjectDao();
+        dao.delete(cd);
+
+        // 一覧へ戻る
+        response.sendRedirect("SubjectList.action");
+    }
+}
